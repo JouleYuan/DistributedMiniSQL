f596905bc6aba60cca97c2a68f675b0e0ec8ca6f
JouleYuan <yuanhaoran0318@gmail.com>
2021-05-10 11:39:08 +0800

    modify region server's path into relative path

--- distributed-minisql-region-server/src/main/java/cn/edu/zju/minisql/distributed/server/region/RegionServer.java
+++ distributed-minisql-region-server/src/main/java/cn/edu/zju/minisql/distributed/server/region/RegionServer.java
@@ -9,7 +9,7 @@ public class RegionServer {
         System.out.println("Starting the region server...");
         ZookeeperService.register(Config.Zookeeper.ip, Config.Zookeeper.port, Config.Zookeeper.timeout,
                 Config.Zookeeper.baseSleepTime, Config.Zookeeper.maxRetries, Config.Zookeeper.namespace,
-                Config.Thrift.port, System.getProperty("user.dir") + Config.Minisql.path);
+                Config.Thrift.port, Config.Minisql.path);
         ThriftService.exposeInterface(Config.Thrift.port);
     }
 }
