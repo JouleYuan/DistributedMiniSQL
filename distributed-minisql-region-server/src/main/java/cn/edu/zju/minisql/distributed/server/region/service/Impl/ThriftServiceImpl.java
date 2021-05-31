7e35159163ae4d60ae8f996407999b056345850e
JouleYuan <yuanhaoran0318@gmail.com>
2021-05-31 20:48:09 +0800

    fix thrift connection not closed bug in region server

--- distributed-minisql-region-server/src/main/java/cn/edu/zju/minisql/distributed/server/region/service/Impl/ThriftServiceImpl.java
+++ distributed-minisql-region-server/src/main/java/cn/edu/zju/minisql/distributed/server/region/service/Impl/ThriftServiceImpl.java
@@ -99,6 +99,8 @@ public class ThriftServiceImpl implements RegionService.Iface {
                         );
             } catch (TException e) {
                 e.printStackTrace();
+            } finally {
+                regionServer.closeTransport();
             }
             break;
         }
